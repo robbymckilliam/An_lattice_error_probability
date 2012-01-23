ef0313df64b3eaa24e5f39cf4bb7398a8c8bc9fc
Robby McKilliam <robby.mckilliam@gmail.com>
2012-01-23 12:51:29 +1030

    more cleanup

--- plots/montecarlo.sh
+++ plots/montecarlo.sh
@@ -1,2 +1,3 @@
 scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar montecarlo.scala
 scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar znsim.scala
+scala -nocompdaemon -cp PubSim.jar:Jama-1.0.2.jar:flanagan.jar:colt.jar:RngPack.jar e8sim.scala
