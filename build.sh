cf20e9f5f4c74f038a6c6152a757a5c22344bfd2
Robby McKilliam <robby.mckilliam@gmail.com>
2012-02-20 22:07:41 +1030

    modified some srcipts

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
-cd plots
-mpost -interaction=nonstopmode peplots.mp
-cd ..
+cd plots   
+mpost -interaction=nonstopmode peplots.mp   
+cd ..   
 
-latex paper.tex
-bibtex paper
-latex paper.tex
-latex paper.tex
-dvips paper.dvi
-ps2pdf paper.ps
+latex paper.tex  
+bibtex paper  
+latex paper.tex  
+latex paper.tex  
+dvips paper.dvi  
+ps2pdf paper.ps  
