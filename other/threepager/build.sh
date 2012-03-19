85e65e2f93ee00be3111a2487a04d583d695734c
Robby McKilliam <robby.mckilliam@gmail.com>
2012-03-19 11:04:08 +1030

    added modified stuff for the threepager.tex

--- other/threepager/build.sh
+++ other/threepager/build.sh
@@ -3,8 +3,8 @@ mpost -interaction=nonstopmode ../../plots/thpplot.mp
 ruby1.9.1 hexlatfig.rb 
 
 #build tex 
-pdflatex threepager.tex 
+latex threepager.tex 
 bibtex threepager 
-pdflatex threepager.tex 
-pdflatex threepager.tex 
+latex threepager.tex 
+latex threepager.tex 
 
