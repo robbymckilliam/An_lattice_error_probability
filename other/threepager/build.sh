5cbbcae6df4226269ff6ae9a147834e3d08aef38
Robby McKilliam <robby.mckilliam@gmail.com>
2012-04-22 11:52:21 +0930

    Added the final MTNS 3 page version

--- other/threepager/build.sh
+++ other/threepager/build.sh
@@ -3,8 +3,8 @@ mpost -interaction=nonstopmode ../../plots/thpplot.mp
 ruby1.9.1 hexlatfig.rb 
 
 #build tex 
-latex threepager.tex 
+pdflatex threepager.tex 
 bibtex threepager 
-latex threepager.tex 
-latex threepager.tex 
+pdflatex threepager.tex 
+pdflatex threepager.tex 
 
